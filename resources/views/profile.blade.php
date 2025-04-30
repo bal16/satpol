3c2229f596fb213d2335effc1299108dad56d505
asegaf74 <asegaf8990@gmail.com>
2025-04-30 13:02:10 +0700

    added profile ui

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,142 @@
 <x-layout :pageName="'Profile'">
-    Profile
+    <x-header />
+    <main>
+        <div class="flex lg:max-h-64 max-h-32 bg-[#FDFDFD] overflow-hidden justify-center">
+            <span class="flex flex-col justify-center text-center lg:h-64 h-32 gap-15 py-auto">
+                <h2 class="font-[DM_Serif_Text] lg:text-5xl text-xl font-black">Visi</h2>
+                <p>"Semarang Kota Perdagangan dan Jasa yang Hebat Menuju Masyarakat Semakin Sejahtera"</p>
+            </span>
+        </div>
+        <div
+            class="bg-[#2B2A29] max-w-screen lg:max-h-166 flex justify-center lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            <div
+                class="flex flex-col font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
+                <span class="font-bold text-xl lg:text-5xl">Sejarah</span>
+                <span class="font-sans text-xs lg:text-sm">
+                    Polisi Pamong Praja didirikan di Yogyakarta pada tanggal 3 Maret 1950 dengan semboyan Praja Wibawa,
+                    untuk mewadahi sebagian ketugasan pemerintah daerah. Sebenarnya ketugasan ini telah dilaksanakan
+                    pemerintah sejak zaman kolonial. Sebelum menjadi Satuan Polisi Pamong Praja setelah proklamasi
+                    kemerdekaan dimana diawali dengan kondisi yang tidak stabil dan mengancam NKRI, dibentuklah
+                    Detasemen Polisi sebagai Penjaga Keamanan Kapanewon di Yogjakarta sesuai dengan Surat Perintah
+                    Jawatan Praja di Daerah Istimewa Yogyakarta untuk menjaga ketentraman dan ketertiban masyarakat.
+                </span>
+                <div class="group font-sans font-bold h-fit w-fit">
+                    <div class="border border-[#fdfdfd] group-hover:border-[#DD1D23] text-[#fdfdfd] py-1">
+                        <button class="px-2">SEJARAH LENGKAP</button>
+                    </div>
+                </div>
+            </div>
+            <img class="w-40 lg:w-85 h-fit my-auto" src="image/logo.png" alt="" />
+        </div>
+        <div
+            class="bg-[#FDFDFD] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            <div
+                class="hidden lg:flex flex-col max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
+            </div>
+            <div
+                class="flex flex-col lg:max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
+                <span class="font-bold text-xl lg:text-5xl">Struktur Organisasi</span>
+            </div>
+        </div>
+        <div
+            class="bg-[#2B2A29] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            <div
+                class="flex flex-col lg:max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
+                <span class="font-bold text-xl lg:text-5xl">Kegiatan</span>
+                <span class="text-xs lg:text-sm">Dalam usahanya untuk menjaga ketentraman dan ketertiban dalam
+                    masyarakat, Satuan Polisi Pamong Praja melaksanakan kegiatan-kegiatan penertiban yang dapat
+                    mengganggu keamanan dan kenyamanan masyarakat.
+                    Beberapa kegiatan yang dilaksanakan adalah menyangkut hal-hal berikut:
+                    <ul>
+                        <li>
+                            Operasi Yustisi
+                        </li>
+                        <li>
+                            Operasi Rutin
+                        </li>
+                        <li>
+                            Penanganan Unjuk Rasa
+                        </li>
+                    </ul>
+                </span>
+            </div>
+            <div
+                class="hidden lg:flex flex-col max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
+                <span class="text-xs lg:text-sm">
+                    <ul>
+                        <li>
+                            Patroli Wilayah
+                        </li>
+                        <li>
+                            Patroli Pariwisata
+                        </li>
+                        <li>
+                            Kewaspadaan Dini
+                        </li>
+                        <li>
+                            Sosialisasi
+                        </li>
+                        <li>
+                            Bimbingan dan Penyuluhan
+                        </li>
+                        <li>
+                            Posko Kewaspadaan Linmas
+                        </li>
+                        <li>
+                            Patroli keamanan dan kenyamanan lingkungan
+                        </li>
+                        <li>
+                            Mobilisasi dan pergerakan linmas
+                        </li>
+                        <li>
+                            Pengaduan
+                        </li>
+                        <li>
+                            Pengendalian
+                        </li>
+                        <li>
+                            Penyidikan dan penyelidikan
+                        </li>
+                        <li>
+                            Pembinaan PPNS
+                        </li>
+                        <li>
+                            Kegiatan-gegiatan lain
+                        </li>
+                    </ul>
+                </span>
+            </div>
+        </div>
+        <div
+            class="bg-[#FDFDFD] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            <div
+                class="hidden lg:flex flex-col max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
+            </div>
+            <div
+                class="flex flex-col lg:max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
+                <span class="font-bold text-xl lg:text-5xl">Tupoksi</span>
+            </div>
+        </div>
+        <div
+            class="bg-[#2B2A29] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            <div
+                class="flex flex-col lg:max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
+                <span class="font-bold text-xl lg:text-5xl">SOP</span>
+            </div>
+            <div
+                class="hidden lg:flex flex-col max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
+            </div>
+        </div>
+        <div
+            class="bg-[#FDFDFD] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            <div
+                class="hidden lg:flex flex-col max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
+            </div>
+            <div
+                class="flex flex-col lg:max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
+                <span class="font-bold text-xl lg:text-5xl">Strategi & Kebijakan</span>
+            </div>
+        </div>
+    </main>
+    <x-footer />
 </x-layout>
