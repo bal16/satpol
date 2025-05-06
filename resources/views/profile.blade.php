b64f21a4d925ca7e41c84dc66135f769021e8bdd
asegaf74 <asegaf8990@gmail.com>
2025-05-06 14:29:42 +0700

    profile ui fix w & h

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,17 +2,17 @@
     <x-header />
     <main>
         <div class="flex lg:max-h-64 max-h-32 bg-[#FDFDFD] overflow-hidden justify-center">
-            <span class="flex flex-col justify-center text-center lg:h-64 h-32 gap-15 py-auto">
+            <span class="flex flex-col justify-center text-center lg:h-64 h-32 lg:gap-15 gap-5 py-auto">
                 <h2 class="font-[DM_Serif_Text] lg:text-5xl text-xl font-black">Visi</h2>
-                <p>"Semarang Kota Perdagangan dan Jasa yang Hebat Menuju Masyarakat Semakin Sejahtera"</p>
+                <p class="text-xs lg:text-lg">"Semarang Kota Perdagangan dan Jasa yang Hebat Menuju Masyarakat Semakin
+                    Sejahtera"</p>
             </span>
         </div>
-        <div
-            class="bg-[#2B2A29] max-w-screen lg:max-h-166 flex justify-center lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+        <div class="bg-[#2B2A29] lg:max-h-166 flex justify-center lg:gap-35 gap-10 lg:px-20 px-5 lg:py-20 py-5">
             <div
-                class="flex flex-col font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
-                <span class="font-bold text-xl lg:text-5xl">Sejarah</span>
-                <span class="font-sans text-xs lg:text-sm">
+                class="flex flex-col font-[DM_Serif_Text] text-justify justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
+                <span class="font-bold text-2xl lg:text-5xl">Sejarah</span>
+                <span class="font-sans text-xs lg:text-lg">
                     Polisi Pamong Praja didirikan di Yogyakarta pada tanggal 3 Maret 1950 dengan semboyan Praja Wibawa,
                     untuk mewadahi sebagian ketugasan pemerintah daerah. Sebenarnya ketugasan ini telah dilaksanakan
                     pemerintah sejak zaman kolonial. Sebelum menjadi Satuan Polisi Pamong Praja setelah proklamasi
@@ -26,28 +26,30 @@
                     </div>
                 </div>
             </div>
-            <img class="w-40 lg:w-85 h-fit my-auto" src="image/logo.png" alt="" />
+            <img class="w-45 lg:w-75 h-fit my-auto" src="image/logo.png" alt="" />
         </div>
         <div
-            class="bg-[#FDFDFD] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            class="bg-[#FDFDFD] lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-20 px-5 lg:py-20 py-5">
             <div
                 class="hidden lg:flex flex-col max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
             </div>
             <div
-                class="flex flex-col lg:max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
-                <span class="font-bold text-xl lg:text-5xl">Struktur Organisasi</span>
+                class="flex flex-col text-center lg:max-w-160 justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
+                <span class="font-[DM_Serif_Text] font-bold text-xl lg:text-5xl">Struktur Organisasi</span>
             </div>
         </div>
         <div
-            class="bg-[#2B2A29] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
-            <div
-                class="flex flex-col lg:max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
-                <span class="font-bold text-xl lg:text-5xl">Kegiatan</span>
-                <span class="text-xs lg:text-sm">Dalam usahanya untuk menjaga ketentraman dan ketertiban dalam
-                    masyarakat, Satuan Polisi Pamong Praja melaksanakan kegiatan-kegiatan penertiban yang dapat
-                    mengganggu keamanan dan kenyamanan masyarakat.
-                    Beberapa kegiatan yang dilaksanakan adalah menyangkut hal-hal berikut:
-                    <ul>
+            class="bg-[#2B2A29] lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-20 px-5 lg:py-20 py-5">
+            <div class="flex flex-col gap-5">
+                <div
+                    class="flex flex-col text-center text-xs lg:text-lg justify-center text-[#FDFDFD] px-5 lg:gap-10 gap-5">
+                    <span class="font-[DM_Serif_Text] font-bold text-xl lg:text-5xl">Kegiatan</span>
+                    <span>Dalam usahanya untuk menjaga ketentraman dan
+                        ketertiban dalam masyarakat, Satuan Polisi Pamong Praja melaksanakan kegiatan-kegiatan
+                        penertiban yang dapat mengganggu keamanan dan kenyamanan masyarakat. Beberapa kegiatan yang
+                        dilaksanakan adalah menyangkut hal-hal berikut:
+                    </span>
+                    <ul class="columns-2 text-start">
                         <li>
                             Operasi Yustisi
                         </li>
@@ -57,13 +59,6 @@
                         <li>
                             Penanganan Unjuk Rasa
                         </li>
-                    </ul>
-                </span>
-            </div>
-            <div
-                class="hidden lg:flex flex-col max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
-                <span class="text-xs lg:text-sm">
-                    <ul>
                         <li>
                             Patroli Wilayah
                         </li>
@@ -101,14 +96,14 @@
                             Pembinaan PPNS
                         </li>
                         <li>
-                            Kegiatan-gegiatan lain
+                            Kegiatan-kegiatan lain
                         </li>
                     </ul>
-                </span>
+                </div>
             </div>
         </div>
         <div
-            class="bg-[#FDFDFD] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            class="bg-[#FDFDFD] lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-20 px-5 lg:py-20 py-5">
             <div
                 class="hidden lg:flex flex-col max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
             </div>
@@ -118,7 +113,7 @@
             </div>
         </div>
         <div
-            class="bg-[#2B2A29] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            class="bg-[#2B2A29] lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-20 px-5 lg:py-20 py-5">
             <div
                 class="flex flex-col lg:max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#FDFDFD] px-5 lg:gap-10 gap-5">
                 <span class="font-bold text-xl lg:text-5xl">SOP</span>
@@ -128,7 +123,7 @@
             </div>
         </div>
         <div
-            class="bg-[#FDFDFD] max-w-screen lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-40 px-20 lg:py-20 py-10">
+            class="bg-[#FDFDFD] lg:max-h-166 max-h-83 flex justify-between lg:gap-35 gap-10 lg:px-20 px-5 lg:py-20 py-5">
             <div
                 class="hidden lg:flex flex-col max-w-160 font-[DM_Serif_Text] justify-center text-xs lg:text-xl text-[#2B2A29] px-5 lg:gap-10 gap-5">
             </div>
