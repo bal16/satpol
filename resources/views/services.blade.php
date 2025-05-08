a04c5a8409c1c4cb5ede7b3f56eb86365b013c4b
asegaf74 <asegaf8990@gmail.com>
2025-05-09 00:17:26 +0700

    added service ui

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,3 +1,111 @@
 <x-layout :pageName="'Services'">
-    Services
+    <x-header />
+    <main class="flex flex-col items-center">
+        <div class="flex font-bold w-full text-[#FDFDFD] text-4xl font-[DM_Serif_Text] justify-center bg-[#2B2A29]">
+            <h1 class="w-7xl px-26 pt-43">
+                Informasi Umum
+            </h1>
+        </div>
+        <div class="flex flex-col bg-[#FDFDFD] w-7xl py-17 px-23.5">
+            <div class="grid grid-cols-3 gap-14">
+                <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
+                    <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
+                    <span class="flex flex-col p-5 gap-8">
+                        <h3 class="font-bold font-[DM_Serif_Text] text-2xl text-[#E94B23]">Layanan</h3>
+                        <hr class="w-33.5 border-2 border-[#E94B23]">
+                        <ul class="text-[#2B2A29] font-bold font-[DM_Serif_Text] max-w-71.5 list-disc px-7">
+                            <li><a href="">Sarana dan Prasarana</a></li>
+                            <li><a href="">Daftar Informasi Publik</a></li>
+                            <li><a href="">Peraturan</a></li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
+                    <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
+                    <span class="flex flex-col p-5 gap-8">
+                        <h3 class="font-bold font-[DM_Serif_Text] text-2xl text-[#E94B23]">Pemberdayaan</h3>
+                        <hr class="w-33.5 border-2 border-[#E94B23]">
+                        <ul class="text-[#2B2A29] font-bold font-[DM_Serif_Text] max-w-71.5 list-disc px-7">
+                            <li><a href="">Sarana dan Prasarana</a></li>
+                            <li><a href="">Daftar Informasi Publik</a></li>
+                            <li><a href="">Peraturan</a></li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
+                    <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
+                    <span class="flex flex-col p-5 gap-8">
+                        <h3 class="font-bold font-[DM_Serif_Text] text-2xl text-[#E94B23]">PPID</h3>
+                        <hr class="w-33.5 border-2 border-[#E94B23]">
+                        <ul class="text-[#2B2A29] font-bold font-[DM_Serif_Text] max-w-71.5 list-disc px-7">
+                            <li><a href="">Sarana dan Prasarana</a></li>
+                            <li><a href="">Daftar Informasi Publik</a></li>
+                            <li><a href="">Peraturan</a></li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
+                    <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
+                    <span class="flex flex-col p-5 gap-8">
+                        <h3 class="font-bold font-[DM_Serif_Text] text-2xl text-[#E94B23]">Kontak</h3>
+                        <hr class="w-33.5 border-2 border-[#E94B23]">
+                        <ul class="text-[#2B2A29] font-bold font-[DM_Serif_Text] max-w-71.5 list-disc px-7">
+                            <li><a href="">Sarana dan Prasarana</a></li>
+                            <li><a href="">Daftar Informasi Publik</a></li>
+                            <li><a href="">Peraturan</a></li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
+                    <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
+                    <span class="flex flex-col p-5 gap-8">
+                        <h3 class="font-bold font-[DM_Serif_Text] text-2xl text-[#E94B23]">Perda Kota Semarang</h3>
+                        <hr class="w-33.5 border-2 border-[#E94B23]">
+                        <ul class="text-[#2B2A29] font-bold font-[DM_Serif_Text] max-w-71.5 list-disc px-7">
+                            <li><a href="">Sarana dan Prasarana</a></li>
+                            <li><a href="">Daftar Informasi Publik</a></li>
+                            <li><a href="">Peraturan</a></li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
+                    <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
+                    <span class="flex flex-col p-5 gap-8">
+                        <h3 class="font-bold font-[DM_Serif_Text] text-2xl text-[#E94B23]">SOP</h3>
+                        <hr class="w-33.5 border-2 border-[#E94B23]">
+                        <ul class="text-[#2B2A29] font-bold font-[DM_Serif_Text] max-w-71.5 list-disc px-7">
+                            <li><a href="">Sarana dan Prasarana</a></li>
+                            <li><a href="">Daftar Informasi Publik</a></li>
+                            <li><a href="">Peraturan</a></li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
+                    <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
+                    <span class="flex flex-col p-5 gap-8">
+                        <h3 class="font-bold font-[DM_Serif_Text] text-2xl text-[#E94B23]">SOP</h3>
+                        <hr class="w-33.5 border-2 border-[#E94B23]">
+                        <ul class="text-[#2B2A29] font-bold font-[DM_Serif_Text] max-w-71.5 list-disc px-7">
+                            <li><a href="">Sarana dan Prasarana</a></li>
+                            <li><a href="">Daftar Informasi Publik</a></li>
+                            <li><a href="">Peraturan</a></li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
+                    <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
+                    <span class="flex flex-col p-5 gap-8">
+                        <h3 class="font-bold font-[DM_Serif_Text] text-2xl text-[#E94B23]">SOP</h3>
+                        <hr class="w-33.5 border-2 border-[#E94B23]">
+                        <ul class="text-[#2B2A29] font-bold font-[DM_Serif_Text] max-w-71.5 list-disc px-7">
+                            <li><a href="">Sarana dan Prasarana</a></li>
+                            <li><a href="">Daftar Informasi Publik</a></li>
+                            <li><a href="">Peraturan</a></li>
+                        </ul>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </main>
+    <x-footer />
 </x-layout>
