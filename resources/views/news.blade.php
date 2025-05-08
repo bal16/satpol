3fd1a0206ad776e200cadc45ebf6ace7d1a86b5c
asegaf74 <asegaf8990@gmail.com>
2025-05-08 10:57:29 +0700

    fix ui news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -2,63 +2,63 @@
     <x-header />
     <main class="static bg-cover lg:min-h-166 min-h-83" style="background-image:url(image/slider.png)">
         <div class="static w-full h-full bg-gradient-to-b from-white-100/0 to-[#FDFDFD]">
-            <div class="flex flex-col p-20 pt-65 gap-5">
-                <h1 class="text-5xl font-[DM_Serif_Text] text-[#FDFDFD]">Berita</h1>
+            <div class="flex flex-col lg:max-w-320 max-w-200 mx-auto p-20 lg:pt-65 gap-5">
+                <h1 class="lg:text-5xl text-3xl font-[DM_Serif_Text] text-[#FDFDFD]">Berita</h1>
                 <div class="flex rounded-sm bg-[#FDFDFD]">
-                    <div class="flex flex-col lg:max-w-180 max-w-160 p-8 gap-6">
-                        <div class="flex lg:max-w-160 max-w-120 justify-between group gap-10">
-                            <a class="z-10 absolute lg:w-141 w-121 lg:h-31.5 h-22.5" href=""></a>
+                    <div class="flex flex-col lg:max-w-180 max-w-120 lg:py-8 py-5 lg:px-15 px-9 gap-6">
+                        <div class="flex lg:max-w-160 max-w-100 justify-between group gap-10">
+                            <a class="z-10 absolute lg:w-160 w-100 lg:h-35 h-22.5" href=""></a>
                             <div style="background-image:url(image/tes.png)"
-                                class="bg-cover rounded max-w-56 lg:w-56 w-36.5 max-h-31.5 lg:h-31.5 h-22.5">
+                                class="bg-cover rounded lg:w-63.5 w-39.5 lg:h-35 h-22">
                             </div>
-                            <div class="flex flex-col max-w-75 justify-center">
+                            <div class="flex flex-col lg:max-w-85 max-w-53 justify-center">
                                 <div class="flex divide-x gap-2 lg:text-xs text-[8px]">
                                     <a class="z-20 hover:underline inline-block pe-2" href="#kontol">Berita</a>
                                     <span>22 April 2025</span>
                                 </div>
                                 <span
-                                    class="lg:text-sm text-xs group-hover:border-l-3 group-hover:border-[#e93b23] group-hover:ps-2">Lorem
+                                    class="lg:text-sm text-[10px] group-hover:border-l-3 group-hover:border-[#e93b23] group-hover:ps-2">Lorem
                                     ipsum dolor sit, amet consectetur adipisicing
                                     elit. Officia aliquam dignissimos voluptatum voluptatibus perspiciatis
                                     cupiditate ut at, facere quod vel.</span>
                             </div>
                         </div>
-                        <div class="flex lg:max-w-160 max-w-120 justify-between group gap-10">
-                            <a class="z-10 absolute lg:w-141 w-121 lg:h-31.5 h-22.5" href=""></a>
+                        <div class="flex lg:max-w-160 max-w-100 justify-between group gap-10">
+                            <a class="z-10 absolute lg:w-160 w-100 lg:h-35 h-22.5" href=""></a>
                             <div style="background-image:url(image/tes.png)"
-                                class="bg-cover rounded max-w-56 lg:w-56 w-36.5 max-h-31.5 lg:h-31.5 h-22.5">
+                                class="bg-cover rounded lg:w-63.5 w-39.5 lg:h-35 h-22">
                             </div>
-                            <div class="flex flex-col max-w-75 justify-center">
+                            <div class="flex flex-col lg:max-w-85 max-w-53 justify-center">
                                 <div class="flex divide-x gap-2 lg:text-xs text-[8px]">
                                     <a class="z-20 hover:underline inline-block pe-2" href="#kontol">Berita</a>
                                     <span>22 April 2025</span>
                                 </div>
                                 <span
-                                    class="lg:text-sm text-xs group-hover:border-l-3 group-hover:border-[#e93b23] group-hover:ps-2">Lorem
+                                    class="lg:text-sm text-[10px] group-hover:border-l-3 group-hover:border-[#e93b23] group-hover:ps-2">Lorem
                                     ipsum dolor sit, amet consectetur adipisicing
                                     elit. Officia aliquam dignissimos voluptatum voluptatibus perspiciatis
                                     cupiditate ut at, facere quod vel.</span>
                             </div>
                         </div>
-                        <div class="flex lg:max-w-160 max-w-120 justify-between group gap-10">
-                            <a class="z-10 absolute lg:w-141 w-121 lg:h-31.5 h-22.5" href=""></a>
+                        <div class="flex lg:max-w-160 max-w-100 justify-between group gap-10">
+                            <a class="z-10 absolute lg:w-160 w-100 lg:h-35 h-22.5" href=""></a>
                             <div style="background-image:url(image/tes.png)"
-                                class="bg-cover rounded max-w-56 lg:w-56 w-36.5 max-h-31.5 lg:h-31.5 h-22.5">
+                                class="bg-cover rounded lg:w-63.5 w-39.5 lg:h-35 h-22">
                             </div>
-                            <div class="flex flex-col max-w-75 justify-center">
+                            <div class="flex flex-col lg:max-w-85 max-w-53 justify-center">
                                 <div class="flex divide-x gap-2 lg:text-xs text-[8px]">
                                     <a class="z-20 hover:underline inline-block pe-2" href="#kontol">Berita</a>
                                     <span>22 April 2025</span>
                                 </div>
                                 <span
-                                    class="lg:text-sm text-xs group-hover:border-l-3 group-hover:border-[#e93b23] group-hover:ps-2">Lorem
+                                    class="lg:text-sm text-[10px] group-hover:border-l-3 group-hover:border-[#e93b23] group-hover:ps-2">Lorem
                                     ipsum dolor sit, amet consectetur adipisicing
                                     elit. Officia aliquam dignissimos voluptatum voluptatibus perspiciatis
                                     cupiditate ut at, facere quod vel.</span>
                             </div>
                         </div>
                         <div class="flex justify-center p-5">
-                            <ul class="flex gap-4 text-xl">
+                            <ul class="flex gap-4 lg:text-xl text-sm">
                                 <li class="opacity-25"><a href="">&lt;</a>
                                 </li>
                                 <li class="text-[#e93b23]"><a href="">1</a>
@@ -75,8 +75,57 @@
                             </ul>
                         </div>
                     </div>
-                    <div>
-
+                    <div class="hidden lg:flex flex-col lg:max-w-140 max-w-87.5 gap-5 items-center">
+                        <div class="mt-8 mx-8 lg:w-75 border-b">
+                            <span class="bg-[#2B2A29] text-[#FDFDFD] text-sm border-b py-1 px-4">
+                                Berita Terbaru
+                            </span>
+                        </div>
+                        <div class="flex lg:max-w-75 justify-between group gap-5.5">
+                            <a class="z-10 absolute lg:w-75 lg:h-17" href=""></a>
+                            <div style="background-image:url(image/tes.png)" class="bg-cover rounded lg:w-30 lg:h-16">
+                            </div>
+                            <div class="flex flex-col lg:max-w-40 justify-center">
+                                <div class="flex divide-x gap-2 lg:text-[11px]">
+                                    <a class="z-20 hover:underline inline-block pe-2" href="#kontol">Berita</a>
+                                    <span>22 April 2025</span>
+                                </div>
+                                <span
+                                    class="lg:text-[10px] group-hover:border-l-3 group-hover:border-[#e93b23] group-hover:ps-2">Lorem
+                                    ipsum dolor sit, amet consectetur adipisicing
+                                    elit. Officia aliquam.</span>
+                            </div>
+                        </div>
+                        <div class="flex lg:max-w-75 justify-between group gap-5.5">
+                            <a class="z-10 absolute lg:w-75 lg:h-17" href=""></a>
+                            <div style="background-image:url(image/tes.png)" class="bg-cover rounded lg:w-30 lg:h-16">
+                            </div>
+                            <div class="flex flex-col lg:max-w-40 justify-center">
+                                <div class="flex divide-x gap-2 lg:text-[11px]">
+                                    <a class="z-20 hover:underline inline-block pe-2" href="#kontol">Berita</a>
+                                    <span>22 April 2025</span>
+                                </div>
+                                <span
+                                    class="lg:text-[10px] group-hover:border-l-3 group-hover:border-[#e93b23] group-hover:ps-2">Lorem
+                                    ipsum dolor sit, amet consectetur adipisicing
+                                    elit. Officia aliquam.</span>
+                            </div>
+                        </div>
+                        <div class="flex lg:max-w-75 justify-between group gap-5.5">
+                            <a class="z-10 absolute lg:w-75 lg:h-17" href=""></a>
+                            <div style="background-image:url(image/tes.png)" class="bg-cover rounded lg:w-30 lg:h-16">
+                            </div>
+                            <div class="flex flex-col lg:max-w-40 justify-center">
+                                <div class="flex divide-x gap-2 lg:text-[11px]">
+                                    <a class="z-20 hover:underline inline-block pe-2" href="#kontol">Berita</a>
+                                    <span>22 April 2025</span>
+                                </div>
+                                <span
+                                    class="lg:text-[10px] group-hover:border-l-3 group-hover:border-[#e93b23] group-hover:ps-2">Lorem
+                                    ipsum dolor sit, amet consectetur adipisicing
+                                    elit. Officia aliquam.</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
