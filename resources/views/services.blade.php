e2ad616eeb7799da18ca726259be0b439266487c
asegaf74 <asegaf8990@gmail.com>
2025-05-12 13:52:09 +0700

    done all figma ui (exc. post etc. & not fixed at service page)

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -6,7 +6,7 @@
                 Informasi Umum
             </h1>
         </div>
-        <div class="flex flex-col bg-[#FDFDFD] w-7xl py-17 px-23.5">
+        <div class="flex flex-col bg-[#FDFDFD] w-7xl py-17 px-23.5 gap-14">
             <div class="grid grid-cols-3 gap-14">
                 <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
                     <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
@@ -80,6 +80,8 @@
                         </ul>
                     </span>
                 </div>
+            </div>
+            <div class="flex bg-[#FDFDFD] justify-center gap-14">
                 <div class="flex flex-col shadow-lg rounded w-81.5 h-201 border-t-5 border-[#E94B23]">
                     <img src="image/tes.png" alt="Thumbnail" class="max-h-54.5 object-cover">
                     <span class="flex flex-col p-5 gap-8">
@@ -104,6 +106,42 @@
                         </ul>
                     </span>
                 </div>
+            </div>
+            <div class="flex flex-col shadow-lg rounded w-273 h-245 border-t-5 border-[#E94B23]">
+                <span class="flex flex-col p-5 pt-10 gap-8">
+                    <h3 class="font-bold font-[DM_Serif_Text] text-5xl text-[#E94B23]">Sejarah Lengkap</h3>
+                    <hr class="w-95.5 border-2 border-[#E94B23]">
+                    <p class="text-2xl text-justify font-[DM_Serif_Text] font-bold w-253">
+                        Satuan Polisi Pamong Praja, disingkat Satpol PP, adalah perangkat Pemerintah Daerah dalam
+                        memelihara ketentraman dan ketertiban umum serta menegakkan Peraturan Daerah. Organisasi dan
+                        tata kerja Satuan Polisi Pamong Praja ditetapkan dengan Peraturan Daerah.</br></br>
+                        Polisi Pamong Praja didirikan di Yogyakarta pada tanggal 3 Maret 1950 dengan semboyan Praja
+                        Wibawa, untuk mewadahi sebagian ketugasan pemerintah daerah. Sebenarnya ketugasan ini telah
+                        dilaksanakan pemerintah sejak zaman kolonial. Sebelum menjadi Satuan Polisi Pamong Praja setelah
+                        proklamasi kemerdekaan dimana diawali dengan kondisi yang tidak stabil dan mengancam NKRI,
+                        dibentuklah Detasemen Polisi sebagai Penjaga Keamanan Kapanewon di Yogjakarta sesuai dengan
+                        Surat Perintah Jawatan Praja di Daerah Istimewa Yogyakarta untuk menjaga ketentraman dan
+                        ketertiban masyarakat.</br></br>
+                        Pada tanggal 10 November 1948, lembaga ini berubah menjadi Detasemen Polisi Pamong Praja. Di
+                        Jawa dan Madura Satuan Polisi Pamong Praja dibentuk tanggal 3 Maret 1950. Inilah awal mula
+                        terbentuknya Satpol PP. dan oleh sebab itu, setiap tanggal 3 Maret ditetapkan sebagai Hari Jadi
+                        Satuan Polisi Pamong Praja (Satpol PP) dan diperingati setiap tahun. Pada Tahun 1960, dimulai
+                        pembentukan Kesatuan Polisi Pamong Praja di luar Jawa dan Madura, dengan dukungan para petinggi
+                        militer /Angkatan Perang. Tahun 1962 namanya berubah menjadi Kesatuan Pagar Baya untuk
+                        membedakan dari korps Kepolisian Negara seperti dimaksud dalam UU No 13/1961 tentang Pokok-pokok
+                        Kepolisian. Tahun 1963 berubah nama lagi menjadi Kesatuan Pagar Praja. Istilah Satpol PP mulai
+                        terkenal sejak pemberlakuan UU No 5/1974 tentang Pokok-pokok Pemerintahan di Daerah.
+                    </p>
+                </span>
+            </div>
+            <div class="flex flex-col shadow-lg rounded w-273 h-245 border-t-5 border-[#E94B23]">
+                <span class="flex flex-col p-5 pt-10 gap-8">
+                    <h3 class="font-bold font-[DM_Serif_Text] text-5xl text-[#E94B23]">Map</h3>
+                    <hr class="w-95.5 border-2 border-[#E94B23]">
+                    <iframe class="mx-auto w-238 h-185"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.247579238948!2d110.38884333955568!3d-6.980085989247468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b2d9507a13b%3A0xf137c4f77567b38c!2sKantor+Satpol+PP!5e0!3m2!1sid!2sid!4v1523937750085"
+                        style="border:0" width="100%" data-ruffle-polyfilled=""></iframe>
+                </span>
             </div>
         </div>
     </main>
