a732cacbd3d5fa8a78c7012538ea7ebcecbcc172
rifqieali <rifqieali48@gmail.com>
2025-05-23 16:47:31 +0700

    add:middleware.last

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Middleware\AdminAuth;
 
 
-
-Route::prefix('/admin')->group(function () {
+Route::prefix('/admin')->middleware([AdminAuth::class])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-})->middleware([]);
+});
+
+// use App\Http\Middleware\AdminAuth;
+
+
+
