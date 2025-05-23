32727c2f41a661d7dbe78167f09b5063cd9e7edb
rifqieali <rifqieali48@gmail.com>
2025-05-23 14:50:26 +0700

    rute web

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,14 @@ Route::prefix('/info')
             return view('sop');
         });
     });
+
+use App\Http\Middleware\AdminAuth;
+use App\Http\Controllers\AdminController;
+
+// Route::get('/admin/login', [AdminController::class, 'loginForm'])->name('admin.login');
+// Route::post('/admin/login', [AdminController::class, 'login']);
+
+// Route::middleware([AdminAuth::class])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::post('/admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
+// });
