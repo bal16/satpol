9756ec73c822f9e85554d54393b7af671f07324e
bal16 <ahmadhamidbalya@gmail.com>
2025-05-26 13:05:23 +0700

    chore: change admin dashboard

--- routes/admin.php
+++ routes/admin.php
@@ -7,12 +7,9 @@ use App\Http\Middleware\AdminAuth;
 
 
 Route::prefix('/admin')->middleware([AdminAuth::class])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
     Route::get('/news', [NewsController::class, 'index'])->name('admin.news');
     Route::get('/gallery', [GalleryController::class, 'index'])->name('admin.gallery');
 });
 
 // use App\Http\Middleware\AdminAuth;
-
-
-
