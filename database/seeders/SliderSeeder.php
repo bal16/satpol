3d6c0bef984d96d5e9cb2f17872e4e8868b862a2
bal16 <ahmadhamidbalya@gmail.com>
2025-06-02 11:26:27 +0700

    feat: slider seeder

--- database/seeders/SliderSeeder.php
+++ database/seeders/SliderSeeder.php
@@ -4,21 +4,28 @@ namespace Database\Seeders;
 
 use App\Models\Slider;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SliderSeeder extends Seeder
 {
-    // use Helper;
+    use Helper;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
+        $this->unzip("sliders.zip", "sliders");
+
         for ($i = 1; $i <= 5; $i++) {
+            $storagePath = $this->process_image("$i.webp", "", 'sliders');
             Slider::factory()->create([
                 'slot_number' => $i,
-                'image_path' => ''
+                'image_path' => $storagePath
             ]);
         }
+
+        File::deleteDirectory($this->dataPath("images"));
+
     }
 }
