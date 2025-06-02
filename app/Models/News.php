c00df61a6a47a6b9e6da272ae0ca77c035012f92
bal16 <ahmadhamidbalya@gmail.com>
2025-06-02 14:16:15 +0700

    fix: id issue

--- app/Models/News.php
+++ app/Models/News.php
@@ -26,10 +26,10 @@ class News extends Model
         'slug',
     ];
 
-    public function getRouteKeyName(): string
-    {
-        return 'slug';
-    }
+    // public function getRouteKeyName(): string
+    // {
+    //     return 'slug';
+    // }
 
     public function images()
     {
