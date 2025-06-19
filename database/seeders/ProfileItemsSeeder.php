558bfe241f4b13ee64317ea25d4f73045c4fc96b
bal16 <ahmadhamidbalya@gmail.com>
2025-06-19 13:17:02 +0700

    fix: seeder issue

--- database/seeders/ProfileItemsSeeder.php
+++ database/seeders/ProfileItemsSeeder.php
@@ -15,12 +15,13 @@ class ProfileItemsSeeder extends Seeder
     public function run(): void
     {
         $this->makeCollectionfromJSON($this->dataPath("profiles.json"))->each(function ($item) {
+            $type = $item->type ?? "text";
             ProfileItems::create([
                 'title' => $item->title,
-                'type' => $item->type ?? "text",
+                'type' => $type,
                 'content' => $item->content ?? null,
                 'show' => $item->show ?? false,
-                'html' => $item->type == "html" ? $item->content : null,
+                'body' => $type == "html" ? $item->body : null,
             ]);
         });
     }
